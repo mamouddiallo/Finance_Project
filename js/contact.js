7e4e67801aa459bf32070aa1ec789e94cc250b51
mamouddiallo <diallomamoud298@gmail.com>
2023-09-28 09:21:09 +0000

    gestion du formulaire contact

--- js/contact.js
+++ js/contact.js
@@ -1,21 +1,23 @@
-let btnsubmit = document.querySelector('.soumission');
-    btnsubmit.addEventListener("click",()=>{
-        let database  = JSON.parse(localStorage.getItem('database')||[])
-        let name  = document.querySelector('#name').value;
-        let email  = document.querySelector('#email').value;
-        let subject = document.querySelector('#subject').value;
-        let message = document.querySelector('#message').value;
-        console.log("mamoud")
-
-        let user = {
-                name:name,
-                email:email,
-                subject:subject,
-                message:message
-        }
-        database.push(user)
-        localStorage.setItem("database",JSON.stringify(database));
-        alert( "Thanks  " + name + " for your informations");
-        window.location.href = "index.html"
+let bouton = document.querySelector('.soumission');
+    bouton .addEventListener("click",(e)=>{
+        e.preventDefault()
+    let database = JSON.parse(localStorage.getItem("database"))||[]
+    let name = document.querySelector("#name").value;
+    let email = document.querySelector("#email").value;
+    let subject = document.querySelector("#subject").value;
+    let message = document.querySelector("#message").value
+    let user = {
+        name : name,
+        email: email,
+        subject:subject,
+        message:message
+    }
+    database.push(user)
+    localStorage.setItem("database",JSON.stringify(database));
+    alert(" Hello,"+name+"thank you for your message. We will contact you shortly")
+    window.location.href='../index.html'
     });
+
+
+
 
