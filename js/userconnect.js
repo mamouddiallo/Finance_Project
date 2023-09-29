d3136ae9ba688391cdc9fd2bae408a3152ecbf06
mamouddiallo <diallomamoud298@gmail.com>
2023-09-29 01:05:16 +0000

    modification

--- js/userconnect.js
+++ js/userconnect.js
@@ -10,20 +10,20 @@ if (userData && userData.firstName) {
   // Affiche le nom de l'utilisateur dans la balise <p>
   user.innerHTML = `<p>Welcome, ${userData.firstName}</p>`;
 }
-// Sélectionnez le lien de déconnexion
+// Sélectionne le lien de déconnexion
 const linkDeconnexion = document.querySelector(".deconnexion");
 
-// Ajoutez un gestionnaire d'événements au clic sur le lien de déconnexion
+// Ajoute un gestionnaire d'événements au clic sur le lien de déconnexion
 linkDeconnexion.addEventListener("click", (e) => {
-  // Empêchez le lien de déclencher directement la redirection
+  // Empêche le lien de déclencher directement la redirection
   e.preventDefault();
 
-  // Affichez une boîte de dialogue de confirmation
+  // Affiche une boîte de dialogue de confirmation
   const confirmation = confirm("Are you sure you want to logout ?");
 
   // Si l'utilisateur confirme la déconnexion, redirigez-le
   if (confirmation) {
-    // Supprimer les données de session dans le localStorage
+    // Supprime les données de session dans le localStorage
     localStorage.removeItem("session");
 
     // Rediriger l'utilisateur vers la page de connexion ou une autre page appropriée
